b0e05d2dd177157de9963890dbd1eec53b14b8b8
ghdnjs18 <ghdnjs18@naver.com>
2021-12-17 16:30:03 +0900

    memberVO 객체 구현

---
+++ src/main/java/com/won/webprj/member/service/MemberVO.java
@@ -0,0 +1,16 @@
+package com.won.webprj.member.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberVO {
+	private String id;
+	private String password;
+	private String name;
+	private String age;
+	private String tel;
+	private String address;
+	private String author;
+}
