20089b61afd19fed81c203d32fc1ac39da28caa8
ghdnjs18 <ghdnjs18@naver.com>
2022-01-18 16:09:10 +0900

    MemberMapper 추가

---
+++ src/main/java/com/won/webprj/member/service/MemberMapper.java
@@ -0,0 +1,12 @@
+package com.won.webprj.member.service;
+
+import java.util.List;
+
+public interface MemberMapper {
+	List<MemberVO> memberSelectList();
+	MemberVO memberSelect(MemberVO vo);
+	int memberInsert(MemberVO vo);
+	int memberUpdate(MemberVO vo);
+	int memberDelete(MemberVO vo);
+	boolean memberIdCheck(String id);
+}
